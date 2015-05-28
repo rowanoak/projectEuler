02d940b3a32b5e2f6f5a42c0ffaa24bdabc166a8
rowanoak <hey.its.rhi@gmail.com>
2015-05-28 10:25:37 -0500

    p4 fix

--- p4.py
+++ p4.py
@@ -1,9 +1,11 @@
 from time import time
 start = time()
+num = 0
 for x in range(100,1000):
     for y in range(x,1000):
         if x*y < (999999):
             if (str(x*y) == (str(x*y)[::-1])):
-                num=x*y
+                if num < x*y:
+                    num=x*y
 print(str(num))
 print("--- %s seconds ---" % (time() - start))
