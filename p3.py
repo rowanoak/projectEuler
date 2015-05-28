e894355a7d2aedf639aeb9dd355f487949086d6d
rowanoak <hey.its.rhi@gmail.com>
2015-05-28 09:59:04 -0500

    added timing

--- p3.py
+++ p3.py
@@ -1,10 +1,16 @@
+from time import time
+start = time()
+
 num = 600851475143
+factor = 0
 
 while factor != num:
     factor += 1
     if (num % factor == 0 and factor != 1):
-        print(str(factor))
         num = num/factor
+        ans = factor
         factor = 0
 
-print(str(num/factor))
+print(str(ans))
+print("--- %s seconds ---" % (time() - start))
+
